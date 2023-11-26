67163599ec155524591edeed66dea8a9c6dcc204
MikeWangWZHL <mikewangwzhl@gmail.com>
2023-11-26 21:15:23 +0000

    update VL benchmarks scripts

--- scripts/eval_llava_geo/llavabench.sh
+++ scripts/eval_llava_geo/llavabench.sh
@@ -3,20 +3,41 @@
 source scripts/setup_openai_api_key.sh
 echo $OPENAI_API_KEY
 
-export CUDA_VISIBLE_DEVICES=0
+
+CUDA_ID="$1"
+MODEL_PATH="$2"
+MODEL_BASE="$3"
+OUTPUT_MODEL_NAME="$4"
+
+export CUDA_VISIBLE_DEVICES=$CUDA_ID
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+echo "model path: ${MODEL_PATH}"
+echo "model base: ${MODEL_BASE}"
+echo "output model name: ${OUTPUT_MODEL_NAME}"
 
+# MODEL_PATH="liuhaotian/llava-v1.5-7b"
+# OUTPUT_MODEL_NAME="llava-v1.5-7b"
 
-MODEL_PATH="liuhaotian/llava-v1.5-7b"
-OUTPUT_MODEL_NAME="llava-v1.5-7b"
-
-python llava/eval/eval_llava_geo/model_vqa.py \
-    --model-path $MODEL_PATH \
-    --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
-    --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
-    --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/$OUTPUT_MODEL_NAME.jsonl \
-    --temperature 0 \
-    --conv-mode vicuna_v1
+if [ "$MODEL_BASE" == "None" ]; then
+    echo "Evaluating non-lora models."
+    python llava/eval/eval_llava_geo/model_vqa.py \
+        --model-path $MODEL_PATH \
+        --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+        --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
+        --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/$OUTPUT_MODEL_NAME.jsonl \
+        --temperature 0 \
+        --conv-mode vicuna_v1
+else
+    echo "Evaluating lora models."
+    python llava/eval/eval_llava_geo/model_vqa.py \
+        --model-path $MODEL_PATH \
+        --model-base $MODEL_BASE \
+        --question-file ./playground/data/eval/llava-bench-in-the-wild/questions.jsonl \
+        --image-folder ./playground/data/eval/llava-bench-in-the-wild/images \
+        --answers-file ./playground/data/eval/llava-bench-in-the-wild/answers/$OUTPUT_MODEL_NAME.jsonl \
+        --temperature 0 \
+        --conv-mode vicuna_v1
+fi
 
 mkdir -p playground/data/eval/llava-bench-in-the-wild/reviews
 
--- scripts/eval_llava_geo/mme.sh
+++ scripts/eval_llava_geo/mme.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
-
 source scripts/setup_openai_api_key.sh
 echo $OPENAI_API_KEY
 
-export CUDA_VISIBLE_DEVICES=0
+
+# MODEL_PATH="liuhaotian/llava-v1.5-7b"
+# OUTPUT_MODEL_NAME="llava-v1.5-7b"
+
+CUDA_ID="$1"
+MODEL_PATH="$2"
+MODEL_BASE="$3"
+OUTPUT_MODEL_NAME="$4"
+
+export CUDA_VISIBLE_DEVICES=$CUDA_ID
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 
-
-MODEL_PATH="liuhaotian/llava-v1.5-7b"
-OUTPUT_MODEL_NAME="llava-v1.5-7b"
-
-
-# python llava/eval/model_vqa_loader.py \
-python llava/eval/eval_llava_geo/model_vqa.py \
-    --model-path $MODEL_PATH \
-    --question-file ./playground/data/eval/MME/llava_mme.jsonl \
-    --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
-    --answers-file ./playground/data/eval/MME/answers/$OUTPUT_MODEL_NAME.jsonl \
-    --temperature 0 \
-    --conv-mode vicuna_v1 \
-    --max-new-tokens 128
+if [ "$MODEL_BASE" == "None" ]; then
+    python llava/eval/eval_llava_geo/model_vqa.py \
+        --model-path $MODEL_PATH \
+        --question-file ./playground/data/eval/MME/llava_mme.jsonl \
+        --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
+        --answers-file ./playground/data/eval/MME/answers/$OUTPUT_MODEL_NAME.jsonl \
+        --temperature 0 \
+        --conv-mode vicuna_v1 \
+        --max-new-tokens 128
+else
+    python llava/eval/eval_llava_geo/model_vqa.py \
+        --model-path $MODEL_PATH \
+        --model-base $MODEL_BASE \
+        --question-file ./playground/data/eval/MME/llava_mme.jsonl \
+        --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
+        --answers-file ./playground/data/eval/MME/answers/$OUTPUT_MODEL_NAME.jsonl \
+        --temperature 0 \
+        --conv-mode vicuna_v1 \
+        --max-new-tokens 128
+fi
 
 cd ./playground/data/eval/MME
 
