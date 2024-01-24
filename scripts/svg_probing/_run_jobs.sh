080b00012bd9b9cbd4bd1ba9a12a4e0e2505a0c1
MikeWangWZHL <mikewangwzhl@gmail.com>
2024-01-24 05:18:31 +0000

    scripts for whats_up

--- scripts/svg_probing/_run_jobs.sh
+++ scripts/svg_probing/_run_jobs.sh
@@ -1,3 +1,40 @@
-bash scripts/svg_probing/svg_text_lora.sh
-bash scripts/svg_probing/image_text_lora_from_llava_stage1.sh
-bash scripts/svg_probing/image_text_lora_from_llava_stage2.sh
+### train
+# bash scripts/svg_probing/svg_text_lora.sh
+# # bash scripts/svg_probing/image_text_lora_from_llava_stage1.sh
+# bash scripts/svg_probing/image_text_lora_from_llava_stage2.sh
+
+### eval
+cd /data/wangz3/projects/ecole-gvs-method
+CODE_DIR="/data/wangz3/projects/ecole-gvs-method/third_party/LLaVA"
+CUDA_ID=5
+
+# TASK_NAME="vsr"
+TASK_NAME="whats_up"
+
+# svg-text
+MODEL_BASE="lmsys/vicuna-7b-v1.5"
+MODEL_PATH="${CODE_DIR}/checkpoints/svg_probing/svg_text_lora/${TASK_NAME}"
+MODEL_NAME="svg_text_lora_vicuna_7b_v1.5__${TASK_NAME}"
+TASK_NAME_FULL="svg_probing_svg-text_${TASK_NAME}"
+bash scripts/svg_probing_eval/eval_svg_probing.sh $TASK_NAME_FULL $CUDA_ID $MODEL_BASE $MODEL_PATH $MODEL_NAME
+
+# # svg-image
+# MODEL_BASE="liuhaotian/llava-v1.5-7b"
+# MODEL_PATH="${CODE_DIR}/checkpoints/svg_probing/image_text_lora/${TASK_NAME}_from_llava_stage2"
+# MODEL_NAME="image_text_lora_llava_7b_v1.5_stage2__${TASK_NAME}"
+# TASK_NAME_FULL="svg_probing_image-text_${TASK_NAME}"
+# bash scripts/svg_probing_eval/eval_svg_probing.sh $TASK_NAME_FULL $CUDA_ID $MODEL_BASE $MODEL_PATH $MODEL_NAME
+
+# # original svg-text
+# MODEL_BASE="None"
+# MODEL_PATH="lmsys/vicuna-7b-v1.5"
+# MODEL_NAME="lmsys_vicuna-7b-v1.5"
+# TASK_NAME_FULL="svg_probing_svg-text_${TASK_NAME}"
+# bash scripts/svg_probing_eval/eval_svg_probing.sh $TASK_NAME_FULL $CUDA_ID $MODEL_BASE $MODEL_PATH $MODEL_NAME
+
+# # original image-text
+# MODEL_BASE="None"
+# MODEL_PATH="liuhaotian/llava-v1.5-7b"
+# MODEL_NAME="liuhaotian_llava-v1.5-7b"
+# TASK_NAME_FULL="svg_probing_image-text_${TASK_NAME}"
+# bash scripts/svg_probing_eval/eval_svg_probing.sh $TASK_NAME_FULL $CUDA_ID $MODEL_BASE $MODEL_PATH $MODEL_NAME
--- scripts/svg_probing/image_text_lora_from_llava_stage2.sh
+++ scripts/svg_probing/image_text_lora_from_llava_stage2.sh
@@ -10,49 +10,147 @@ MODEL_TYPE="llava"
 BATCH_SIZE=8 # 16
 GRAD_ACC_STEP=2 # 1
 
-for TASK_NAME in "line_or_angle" "single_angle" "lines" "intersect_horizontal" "clevr_easy"
-do
-    echo "run task: ${TASK_NAME}"
-    DATA_PATH=${DATA_DIR}/svg_probing/${TASK_NAME}/train_image.json
-    OUTPUT_DIR=${CODE_DIR}/checkpoints/svg_probing/image_text_lora/${TASK_NAME}_from_llava_stage2
-    # mm_projector_path=${CODE_DIR}/checkpoints/projectors/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin
+#############################################
+# for TASK_NAME in "line_or_angle" "single_angle" "lines" "intersect_horizontal" "clevr_easy"
+# do
+#     echo "run task: ${TASK_NAME}"
+#     DATA_PATH=${DATA_DIR}/svg_probing/${TASK_NAME}/train_image.json
+#     OUTPUT_DIR=${CODE_DIR}/checkpoints/svg_probing/image_text_lora/${TASK_NAME}_from_llava_stage2
+#     # mm_projector_path=${CODE_DIR}/checkpoints/projectors/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin
 
-    LR=2e-5
-    deepspeed --include localhost:2,7 --master_port 29502  llava/train/train_mem.py \
-        --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-        --deepspeed ${CODE_DIR}/scripts/${DEEPSPEED}.json \
-        --model_name_or_path ${MODEL_PATH} \
-        --version v1 \
-        --data_path ${DATA_PATH} \
-        --image_folder ${DATA_DIR} \
-        --vision_tower openai/clip-vit-large-patch14-336 \
-        --mm_projector_type mlp2x_gelu \
-        --mm_vision_select_layer -2 \
-        --mm_use_im_start_end False \
-        --mm_use_im_patch_token False \
-        --image_aspect_ratio pad \
-        --bf16 True \
-        --output_dir ${OUTPUT_DIR} \
-        --num_train_epochs 1 \
-        --per_device_train_batch_size ${BATCH_SIZE} \
-        --per_device_eval_batch_size 4 \
-        --gradient_accumulation_steps ${GRAD_ACC_STEP} \
-        --evaluation_strategy "no" \
-        --save_strategy "steps" \
-        --save_steps ${SAVE_PER_STEPS} \
-        --save_total_limit 1 \
-        --learning_rate ${LR} \
-        --weight_decay 0. \
-        --warmup_ratio 0.03 \
-        --lr_scheduler_type "cosine" \
-        --logging_steps 1 \
-        --tf32 True \
-        --model_max_length 2048 \
-        --gradient_checkpointing True \
-        --dataloader_num_workers 4 \
-        --lazy_preprocess True \
-        --report_to wandb \
-        --model_type ${MODEL_TYPE}
-        # --pretrain_mm_mlp_adapter ${mm_projector_path} \
-        # --group_by_modality_length True \
-done
+#     LR=2e-5
+#     deepspeed --include localhost:2,7 --master_port 29502  llava/train/train_mem.py \
+#         --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+#         --deepspeed ${CODE_DIR}/scripts/${DEEPSPEED}.json \
+#         --model_name_or_path ${MODEL_PATH} \
+#         --version v1 \
+#         --data_path ${DATA_PATH} \
+#         --image_folder ${DATA_DIR} \
+#         --vision_tower openai/clip-vit-large-patch14-336 \
+#         --mm_projector_type mlp2x_gelu \
+#         --mm_vision_select_layer -2 \
+#         --mm_use_im_start_end False \
+#         --mm_use_im_patch_token False \
+#         --image_aspect_ratio pad \
+#         --bf16 True \
+#         --output_dir ${OUTPUT_DIR} \
+#         --num_train_epochs 1 \
+#         --per_device_train_batch_size ${BATCH_SIZE} \
+#         --per_device_eval_batch_size 4 \
+#         --gradient_accumulation_steps ${GRAD_ACC_STEP} \
+#         --evaluation_strategy "no" \
+#         --save_strategy "steps" \
+#         --save_steps ${SAVE_PER_STEPS} \
+#         --save_total_limit 1 \
+#         --learning_rate ${LR} \
+#         --weight_decay 0. \
+#         --warmup_ratio 0.03 \
+#         --lr_scheduler_type "cosine" \
+#         --logging_steps 1 \
+#         --tf32 True \
+#         --model_max_length 2048 \
+#         --gradient_checkpointing True \
+#         --dataloader_num_workers 4 \
+#         --lazy_preprocess True \
+#         --report_to wandb \
+#         --model_type ${MODEL_TYPE}
+#         # --pretrain_mm_mlp_adapter ${mm_projector_path} \
+#         # --group_by_modality_length True \
+# done
+
+
+#############################################
+
+# # natural image: vsr task
+# TASK_NAME="vsr"
+# echo "run task: ${TASK_NAME}"
+# DATA_PATH=${DATA_DIR}/svg_probing/${TASK_NAME}/train_image.json
+# OUTPUT_DIR=${CODE_DIR}/checkpoints/svg_probing/image_text_lora/${TASK_NAME}_from_llava_stage2
+# # mm_projector_path=${CODE_DIR}/checkpoints/projectors/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin
+# LR=2e-5
+# NUM_EPOCH=3
+# deepspeed --include localhost:2,3 --master_port 29502  llava/train/train_mem.py \
+#     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+#     --deepspeed ${CODE_DIR}/scripts/${DEEPSPEED}.json \
+#     --model_name_or_path ${MODEL_PATH} \
+#     --version v1 \
+#     --data_path ${DATA_PATH} \
+#     --image_folder ${DATA_DIR} \
+#     --vision_tower openai/clip-vit-large-patch14-336 \
+#     --mm_projector_type mlp2x_gelu \
+#     --mm_vision_select_layer -2 \
+#     --mm_use_im_start_end False \
+#     --mm_use_im_patch_token False \
+#     --image_aspect_ratio pad \
+#     --bf16 True \
+#     --output_dir ${OUTPUT_DIR} \
+#     --num_train_epochs $NUM_EPOCH \
+#     --per_device_train_batch_size ${BATCH_SIZE} \
+#     --per_device_eval_batch_size 4 \
+#     --gradient_accumulation_steps ${GRAD_ACC_STEP} \
+#     --evaluation_strategy "no" \
+#     --save_strategy "steps" \
+#     --save_steps ${SAVE_PER_STEPS} \
+#     --save_total_limit 1 \
+#     --learning_rate ${LR} \
+#     --weight_decay 0. \
+#     --warmup_ratio 0.03 \
+#     --lr_scheduler_type "cosine" \
+#     --logging_steps 1 \
+#     --tf32 True \
+#     --model_max_length 2048 \
+#     --gradient_checkpointing True \
+#     --dataloader_num_workers 4 \
+#     --lazy_preprocess True \
+#     --report_to wandb \
+#     --model_type ${MODEL_TYPE}
+#     # --pretrain_mm_mlp_adapter ${mm_projector_path} \
+#     # --group_by_modality_length True \
+
+
+#############################################
+
+TASK_NAME="whats_up"
+echo "run task: ${TASK_NAME}"
+DATA_PATH=${DATA_DIR}/svg_probing/${TASK_NAME}/train_image.json
+OUTPUT_DIR=${CODE_DIR}/checkpoints/svg_probing/image_text_lora/${TASK_NAME}_from_llava_stage2
+# mm_projector_path=${CODE_DIR}/checkpoints/projectors/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin
+LR=2e-5
+NUM_EPOCH=3
+deepspeed --include localhost:5,6 --master_port 29502  llava/train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed ${CODE_DIR}/scripts/${DEEPSPEED}.json \
+    --model_name_or_path ${MODEL_PATH} \
+    --version v1 \
+    --data_path ${DATA_PATH} \
+    --image_folder ${DATA_DIR} \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --bf16 True \
+    --output_dir ${OUTPUT_DIR} \
+    --num_train_epochs $NUM_EPOCH \
+    --per_device_train_batch_size ${BATCH_SIZE} \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps ${GRAD_ACC_STEP} \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps ${SAVE_PER_STEPS} \
+    --save_total_limit 1 \
+    --learning_rate ${LR} \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --model_type ${MODEL_TYPE}
+    # --pretrain_mm_mlp_adapter ${mm_projector_path} \
+    # --group_by_modality_length True \
